cf6c989c6416d88e7e3801a48df4730855734206
Maria Vasquez <mvasqu9@bancodebogota.com.co>
2023-10-06 20:21:25 -0500

    Se agrega relacion padre - hijo, hijo - padre

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -6,15 +6,27 @@ import { Character } from '../interfaces/character.interfaces';
   templateUrl: './main-page.component.html'
 })
 
-export class mainPageComponent {
-  public nameCharacterDBZ: Character[]=[{
-    name:'Krillin',
-    power:1000
-  },{
-    name:'goku',
-    power:9500
-  },{
-    name:'vegeta',
-    power:7500
+export class mainPageComponent{
+  public nameCharacterDBZ: Character[] = [{
+    name: 'Krillin',
+    power: 1000
+  }, {
+    name: 'goku',
+    power: 9500
+  }, {
+    name: 'vegeta',
+    power: 7500
   }];
+
+  public other: Character[] = [{
+    name: 'Truynks',
+    power: 5000
+  }];
+
+  hello: string = "";
+
+
+  test(event:any){
+    this.hello = event;
+  }
 }
